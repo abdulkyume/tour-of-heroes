664b45adbbb87972593e31ccfcb052695c2c769b
Abdul Kyume <abbirtak@gmail.com>
2022-05-27 02:30:42 +0600

    day 2

--- src/app/heroes-detail/heroes-detail.component.ts
+++ src/app/heroes-detail/heroes-detail.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit, Input} from '@angular/core';
-import { Hero } from '../hero';
-@Component({
-  selector: 'app-heroes-detail',
-  templateUrl: './heroes-detail.component.html',
-  styleUrls: ['./heroes-detail.component.css']
-})
-export class HeroesDetailComponent implements OnInit {
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
-  constructor() { }
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './heroes-detail.component.html',
+  styleUrls: [ './heroes-detail.component.css' ]
+})
+export class HeroDetailComponent implements OnInit {
+  hero: Hero | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
 
   ngOnInit(): void {
+    this.getHero();
   }
-  @Input() hero !: Hero;
+
+  getHero(): void {
+    const id = parseInt(this.route.snapshot.paramMap.get('id')!, 10);
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero)
+        .subscribe(() => this.goBack());
+    }
+  }
 }
