a05e93af35a6158f06c921115252daeceacd1e26
Abdul Kyume <abbirtak@gmail.com>
2022-05-29 22:03:09 +0600

    finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroFormComponent } from './hero-form/hero-form.component';
 import { HeroDetailComponent } from './heroes-detail/heroes-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
 const routes: Routes = [
   { path: 'heroes', component: HeroesComponent},
   { path: 'dashboard', component: DashboardComponent},
+  { path: 'heroform', component: HeroFormComponent},
   { path: 'detail/:id', component: HeroDetailComponent},
   { path: '', redirectTo: '/dashboard', pathMatch: 'full'}
 ];
